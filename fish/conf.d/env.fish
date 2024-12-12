abc912d18909181c9838d3c6d27819031f08a877
Massimo Pavoni <maspavoni@gmail.com>
2024-12-12 21:19:00 +0100

    Reorder env + pager

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -2,8 +2,9 @@
 # Environment
 #----------------
 
-set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
-set -x EDITOR /usr/bin/nvim
-set -x SONAR_SCANNER_HOME /opt/sonar-scanner
-set -x RUSTC_WRAPPER /usr/bin/sccache
 set -x CHROME_EXECUTABLE /usr/bin/chromium
+set -x EDITOR /usr/bin/nvim
+set -x PAGER bat
+set -x RUSTC_WRAPPER /usr/bin/sccache
+set -x SONAR_SCANNER_HOME /opt/sonar-scanner
+set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
