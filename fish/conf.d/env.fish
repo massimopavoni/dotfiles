e2ff1ca3116f44c3774a5bda76b30cfe36d0e1b7
Massimo Pavoni <maspavoni@gmail.com>
2025-03-02 15:24:41 +0100

    Librewolf

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -4,7 +4,7 @@
 
 set -x CHROME_EXECUTABLE /usr/bin/chromium
 set -x EDITOR /usr/bin/nvim
-set -x PAGER bat
+set -x PAGER /usr/bin/bat
 set -x RUSTC_WRAPPER /usr/bin/sccache
 set -x SONAR_SCANNER_HOME /opt/sonar-scanner
 set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
